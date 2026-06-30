d52f035c385fdecd06fd313295d6c2d86912208b
abhi <50333572+abhionly009@users.noreply.github.com>
2026-06-30 13:15:15 +0530

    add code to find second largest number

--- src/main/java/array/SecondLargestElementInArray.java
+++ src/main/java/array/SecondLargestElementInArray.java
@@ -1,4 +1,29 @@
 package main.java.array;
 
 public class SecondLargestElementInArray {
+
+    public int findSecondLargeElement(int [] input){
+
+        int firstLarge = Integer.MIN_VALUE;
+        int secondLarge = Integer.MIN_VALUE;
+
+        for (Integer num :input){
+            if (num>firstLarge){
+                secondLarge = firstLarge;
+                firstLarge = num;
+
+            }else if (num>secondLarge && num != firstLarge){
+                secondLarge = num;
+            }
+        }
+
+        return secondLarge;
+    }
+
+    public static void main(String[] args) {
+        SecondLargestElementInArray secondLargestElementInArray = new SecondLargestElementInArray();
+        int [] input  = {1,4,6,3,5};
+        System.out.println(secondLargestElementInArray.findSecondLargeElement(input));
+
+    }
 }
--- src/test/java/array/SecondLargestNumberInArrayTest.java
+++ src/test/java/array/SecondLargestNumberInArrayTest.java
@@ -1,4 +1,57 @@
 package test.java.array;
 
-public class SecondLargestNumberInArray {
+import main.java.array.SecondLargestElementInArray;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SecondLargestNumberInArrayTest {
+
+    SecondLargestElementInArray secondLargestElementInArray;
+
+    @BeforeEach
+    void setup(){
+        secondLargestElementInArray = new SecondLargestElementInArray();
+    }
+
+    @Test
+    void givenInputIsValidThenReturnSecondHighestNumber(){
+        int [] input  = {1,4,6,3,5};
+
+       assertEquals(5,  secondLargestElementInArray.findSecondLargeElement(input));
+
+    }
+
+    @Test
+    void givenInputIsValidWithNegativeNumberThenReturnSecondHighestNumber(){
+        int [] input  = {-5,-8,-6,-3,-5};
+        assertEquals(-5,  secondLargestElementInArray.findSecondLargeElement(input));
+    }
+
+    @Test
+    void givenInputHasSameNumberThenReturn(){
+        int [] input = {2,2,2,2,2};
+        assertEquals(Integer.MIN_VALUE,secondLargestElementInArray.findSecondLargeElement(input));
+    }
+
+    @Test
+    void givenInputHasMaxValue(){
+        int [] input = {Integer.MAX_VALUE,3,5};
+        assertEquals(5,secondLargestElementInArray.findSecondLargeElement(input));
+    }
+
+    @Test
+    void givenInputHasMinValue(){
+        int [] input = {Integer.MIN_VALUE,3,5};
+        assertEquals(3,secondLargestElementInArray.findSecondLargeElement(input));
+    }
+
+    @Test
+    void givenInputContainsZeroValues(){
+        int [] input  = {-5,-8,0,-3,-5};
+        assertEquals(-3,  secondLargestElementInArray.findSecondLargeElement(input));
+    }
+
+
 }
